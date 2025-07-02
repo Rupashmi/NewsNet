6a4733c3a007d1ca4ab284267caf0688e35a0f0f
Rupashmi <rupshmis333@gmail.com>
2025-07-02 18:20:27 +0530

    Update news.js

--- api/news.js
+++ api/news.js
@@ -2,9 +2,9 @@ export default async function handler(req, res) {
   const { category, keyword } = req.query;
   const apiKey = process.env.NEWS_API_KEY;
 
-  const endpoint = keyword
-    ? `https://newsapi.org/v2/everything?q=${encodeURIComponent(keyword)}&apiKey=${apiKey}`
-    : `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`;
+ const endpoint = keyword
+  ? `https://newsapi.org/v2/everything?q=${encodeURIComponent(keyword)}&apiKey=${apiKey}`
+  : `https://newsapi.org/v2/everything?q=${encodeURIComponent(category)}&apiKey=${apiKey}`;
 
   try {
     const response = await fetch(endpoint);

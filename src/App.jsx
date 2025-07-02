6e59c40a0d84eb5b08fe48dc77af0a196476900f
Rupashmi <rupshmis333@gmail.com>
2025-07-02 09:37:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import EnvCheck from './pages/EnvCheck';
 import MySummaries from './pages/MySummaries';
 import Footer from './components/Footer';
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/env-check" element={<EnvCheck />} />
         <Route path="/" element={<Home />} />
         <Route path="/summaries" element={<MySummaries />} />
       </Routes>
